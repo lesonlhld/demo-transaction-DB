0f672edc50d67fb4fdc3936b8122f1e660ca0bbd
lesonlhld <lesonlhld@gmail.com>
2020-12-16 22:06:01 +0700

    updating demo

--- SpringJDBC/database.sql
+++ SpringJDBC/database.sql
@@ -1,8 +1,12 @@
+drop database IF EXISTS `demo-spring-jdbc-transaction`;
+create schema `demo-spring-jdbc-transaction`;
 use `demo-spring-jdbc-transaction`;
 CREATE TABLE `demo-spring-jdbc-transaction`.`user_info` (
   `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  `address` VARCHAR(255) NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `amount` decimal(20) DEFAULT 0,
   PRIMARY KEY (`id`));
   
   select * from `demo-spring-jdbc-transaction`.`user_info`;
+  
+SELECT amount FROM `demo-spring-jdbc-transaction`.`user_info` WHERE id=1;
